500590e196014bdc2408e03c46cbb12db7686016
mohamedsebai <mohamedseabeai@gmail.com>
2023-09-11 23:44:42 +0200

    added paginate for category data

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -13,7 +13,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        $categories = Category::get();
+        $categories = Category::paginate(1);
         return view('admin.categories.index', compact('categories'));
     }
 
--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -57,6 +57,46 @@
         </div>
 
         </div>
+
+
+         <div class="row">
+            <div class="col-12 mt-5">
+                @if ($categories->hasPages())
+                    <nav aria-label="Page navigation">
+                        <ul class="pagination justify-content-center">
+                        <li class="page-item {{ $categories->currentPage() == 1 ? 'disabled' : '' }}">
+                            <a class="page-link" href="{{ $categories->previousPageUrl() }}" aria-label="Previous">
+                            <span class="fa fa-angle-double-left" aria-hidden="true"></span>
+                            <span class="sr-only"> {{ ('lang.Previous') }} </span>
+                            </a>
+                        </li>
+                        @foreach ( $categories->getUrlRange(1, $categories->lastPage()) as $pageLink)
+                        @php
+                            //fuck you php iam mohamed seabeai
+                            $pageString = strstr($pageLink, 'page=' , false);
+                            $rev = strrev($pageString);
+                            $pageNum = strstr($rev, '=' , true);
+                            $pageNum = strrev($pageNum);
+                        @endphp
+                            <li class="page-item {{ substr($pageLink, -1) == $categories->currentPage() ? 'active': '' }}">
+                                <a class="page-link" href="{{ $pageLink }}">{{ $pageNum }}
+                                </a>
+                            </li>
+                        @endforeach
+                        <li class="page-item {{  $categories->currentPage() == $categories->lastPage() ? 'disabled' : '' }}">
+                            <a class="page-link" href="{{ $categories->nextPageUrl() }}" aria-label="Next">
+                            <span class="fa fa-angle-double-right" aria-hidden="true"></span>
+                            <span class="sr-only"> {{ ('lang.Next') }} </span>
+                        </a>
+                        </li>
+                        </ul>
+                    </nav>
+                @endif
+            </div>
+        </div>
+
+
+
     </div>
 
     </div>
